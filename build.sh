5937962d804b47dded1c0b3d7b5653eae21bce79
Timothee Boussus <timothee.boussus@gmail.com>
2025-07-10 09:11:27 +0200

    add mac build

--- build.sh
+++ build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# Install cpanm
-curl -L http://cpanmin.us | perl - --self-upgrade
+# Install cpanm to current directory
+curl -L https://cpanmin.us -o cpanm
+chmod +x cpanm
 
 # Perl modules
 # https://github.com/exiftool/exiftool/blob/master/windows_exiftool
-cpanm Archive::Zip PAR PAR::Packer IO::String
+# Build PAR::Packer from source to ensure compatibility
+./cpanm Archive::Zip PAR IO::String
+
+# Download and build PAR::Packer from source
+wget https://cpan.metacpan.org/authors/id/R/RS/RSCHUPP/PAR-Packer-1.063.tar.gz
+tar -xzf PAR-Packer-1.063.tar.gz
+cd PAR-Packer-1.063
+perl Makefile.PL
+make
+make install
+cd ..
+rm -rf PAR-Packer-1.063 PAR-Packer-1.063.tar.gz
 zip -y -q -r cpanm-log.zip ~/.cpanm/
 
 # Get repo
@@ -17,6 +29,7 @@ rm -rf windows_exiftool html t
 
 # Build
 ARGS=`awk '!/^#/ && !/Win32|Brotli/' pp_build_exe.args | tr '\n' ' '`
+
 pp $ARGS
 mv exiftool.exe ../
 echo 'Built successfully'
